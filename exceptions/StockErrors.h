207386dcde9ee5ab1553f5ca5961724424a75eea
Davut <2005davut55@gmail.com>
2025-12-15 02:01:52 +0200

    Fixing the small typo bug

---
+++ exceptions/StockErrors.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "StockException.h"
+
+class InvalidPriceException : public StockException {
+public:
+    InvalidPriceException()
+        : StockException("Invalid stock price") {}
+};
+
+class InvalidQuantityException : public StockException {
+public:
+    InvalidQuantityException()
+        : StockException("Invalid stock quantity") {}
+};
+
+class InsufficientBalanceException : public StockException {
+public:
+    InsufficientBalanceException()
+        : StockException("Insufficient balance") {}
+};
